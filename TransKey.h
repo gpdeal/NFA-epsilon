52a901e6df78698bb54342d078216e4a615daebc
gpdeal <gpdeal@gmail.com>
2015-03-17 11:01:09 -0700

    Update TransKey.h

--- TransKey.h
+++ TransKey.h
@@ -1,22 +1,47 @@
+//-----------------------------------------------------------------------------
+// Galen Deal
+// CSS 502
+//
+// TransKey
+//
+// Header for TransKey struct. This object serves as a key for a map of
+// transitions which maps the source node and transition character to the
+// destination node.
+
+
 #pragma once
+
+using namespace std;
 
 struct TransKey {
    int source;
    char transitionChar;
 
+   TransKey() {}
+
+   // Constructor which sets the source node and transition char to the passed
+   // int and char
    TransKey(int node, char c) {
       source = node;
       transitionChar = c;
    }
 
-   bool operator==(const TransKey& other) {
-      return (source == other.source && transitionChar == other.transitionChar);
+   // Overloaded == operator. Returns true if the member variables of this
+   // object are equal to those of the TransKey to which it is compared
+   bool operator==(const TransKey& other) const {
+      return (source == other.source 
+         && transitionChar == other.transitionChar);
    }
 
 };
 
 
-/* The following code was adapted from an answer to a question on stackoverflow
+
+
+/*
+Hash function for TransKey object
+
+The following code was adapted from an answer to a question on stackoverflow
 provided by user jogojapan. Original code is here:
 http://stackoverflow.com/questions/17016175/c-unordered-map-using-a-custom-class-type-as-the-key
 */
@@ -25,23 +50,11 @@ namespace std {
 
    template <>
    struct hash<TransKey> {
-      size_t operator()(TransKey* k) const {
-         using std::hash;
+      size_t operator()(const TransKey& k) const {
 
-         return ((hash<int>()(k->source)
-            ^ (hash<char>()(k->transitionChar) << 1)) >> 1);
+         return ((hash<int>()(k.source)
+            ^ (hash<char>()(k.transitionChar) << 1)) >> 1);
       }
    };
 
 }
-
-
-//struct KeyHasher {
-//   std::size_t operator()(TransKey* k) const {
-//      using std::size_t;
-//      using std::hash;
-//
-//      return ((hash<int>()(k->source)
-//         ^ (hash<char>()(k->transitionChar) << 1)) >> 1);
-//   }
-//};
